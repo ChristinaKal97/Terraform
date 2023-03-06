49da0c0a01f8fa06ffac5eefa5a724de5afd1dd7
ChristinaKal97 <kalantzic@cbs.gr>
2023-03-06 15:16:16 +0200

    kubernetes provider and vars

---
+++ provider.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+variable "host" {
+  type = string
+}
+
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+variable "client_certificate" {
+    type = string
+}
+
+variable "client_key" {
+    type = string
+}
+provider "kubernetes" {
+  host = var.host
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  client_certificate = base64decode(var.client_certificate)
+  client_key = base64decode(var.client_key)
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "200Mi"
+            }
+            requests = {
+              memory = "100Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
