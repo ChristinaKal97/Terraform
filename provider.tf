38cc1c96843b36c542c58d3135f97fb3046be045
ChristinaKal97 <126866200+ChristinaKal97@users.noreply.github.com>
2023-03-07 11:13:29 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -23,7 +23,7 @@ variable "client_key" {
     type = string
 }
 provider "kubernetes" {
-  config_path = "C:/Users/kalantzic.VRICOSMOS/.kube/config"
+  config_path = "~/.kube/config"
   host = var.host
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
   client_certificate = base64decode(var.client_certificate)
