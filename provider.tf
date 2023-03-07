4c579f3a091a5cabaf8b29186a11ea0102f5d1e0
ChristinaKal97 <126866200+ChristinaKal97@users.noreply.github.com>
2023-03-07 14:30:19 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -27,6 +27,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
   client_certificate = base64decode(var.client_certificate)
   client_key = base64decode(var.client_key)
+  load_config_file = false
 }
 
 resource "kubernetes_deployment" "nginx" {
